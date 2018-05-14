87cf7c13fd27438d7ca2f77948dbbc55c7ee016a
Windus <huizhimrl@gmail.com>
2018-05-14 14:31:36 +0800

    Site updated: 2018-05-14 14:31:35

--- loveu/js/jq.snow.js
+++ loveu/js/jq.snow.js
@@ -1,15 +1,13 @@
 /**
  * js网页雪花效果jquery插件 
- * 懒人建站 www.51xuediannao.com   整理
- * @see http://workshop.rs
  */
 (function($){
 	
 	$.fn.snow = function(options){
 	
 			var $flake 			= $('<div id="snowbox" />').css({'position': 'absolute', 'top': '-50px'}).html('&hearts;'),
-				documentHeight 	= $(document).height(),
-				documentWidth	= $(document).width(),
+				documentHeight 	= $(document).height()-80,
+				documentWidth	= $(document).width()-60,
 				defaults		= {
 									minSize		: 10,		//雪花的最小尺寸
 									maxSize		: 20,		//雪花的最大尺寸
@@ -19,12 +17,22 @@
 				options			= $.extend({}, defaults, options);
 			
 			var interval		= setInterval( function(){
-				var startPositionLeft 	= Math.random() * documentWidth - 100,
+				var startPositionLeft 	= Math.floor(Math.random()*(documentWidth-0+1)+0),
 				 	startOpacity		= 0.5 + Math.random(),
 					sizeFlake			= options.minSize + Math.random() * options.maxSize,
-					endPositionTop		= documentHeight - 40,
-					endPositionLeft		= startPositionLeft - 100 + Math.random() * 500,
+					endPositionTop		= documentHeight,
+					endPositionLeft		= Math.floor(Math.random()*((startPositionLeft+500)-(startPositionLeft-500)+1)+(startPositionLeft-500)),
 					durationFall		= documentHeight * 10 + Math.random() * 5000;
+
+					if(endPositionLeft>=documentWidth){
+						endPositionLeft = documentWidth;
+					}
+					if(endPositionLeft<0){
+						endPositionLeft = 0;
+					}
+
+					console.log("- :"+startPositionLeft)
+					console.log("=:"+endPositionLeft)
 				$flake.clone().appendTo('body').css({
 							left: startPositionLeft,
 							opacity: startOpacity,
